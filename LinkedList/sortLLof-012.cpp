a7dfff52e1b914c55cf8331548b1104f664cb7c5
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-07-24 22:15:04 +0530

    Create sortLLof-012.cpp

---
+++ LinkedList/sortLLof-012.cpp
@@ -0,0 +1,51 @@
+
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        
+        // Add code here
+        if(head == NULL) return NULL;
+        
+        Node* temp = head;
+       int count[3]={0,0,0};
+        while(temp != NULL)
+        {
+            count[temp->data]++;
+            
+            temp = temp->next;
+        }
+        
+        
+        temp = head;
+     
+         int i = 0;
+          while (temp != NULL) {
+            if (count[i] == 0) {
+              i++;
+            } else {
+              temp->data = i;
+              count[i]--;
+              temp = temp->next;
+            }
+          }
+        return head;
+        
+    }
+};
+
+

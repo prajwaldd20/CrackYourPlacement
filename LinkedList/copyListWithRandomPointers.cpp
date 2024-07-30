58bec7023672d1fe4bdee50b8792f2a3588c7fdc
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-07-30 14:19:23 +0530

    Update copyListWithRandomPointers.cpp

--- LinkedList/copyListWithRandomPointers.cpp
+++ LinkedList/copyListWithRandomPointers.cpp
@@ -30,4 +30,59 @@
 
         return mp[head];
 
+
+  
+        // Approach 2: 
+        //  create node in between two nodes
+
+
+        Node* temp = head;
+
+        while(temp != NULL) 
+        {
+            // create new node
+            Node* p= new Node(temp->val);
+            p->next = temp->next;
+            temp->next = p;
+
+            temp = temp->next->next;
+
+        }
+
+        // connect random pointers
+     
+        temp = head; // copied list
+
+        while(temp != NULL)
+        {
+            // copynode
+            Node* p = temp->next;
+            if(temp->random)
+            p->random = temp->random->next;
+            else
+            p->random = NULL;
+
+            temp = temp->next->next;
+        }
+
+        // connect next pointers
+        Node* dummy = new Node(-1);
+
+        Node* res = dummy;
+
+
+        temp = head;
+
+        while(temp != NULL )
+        {
+            res -> next = temp->next;
+            res =res->next;
+             temp->next = temp->next->next;
+             
+             temp = temp->next;
+           
+        }
+
+        return dummy->next;
+
     }

19a5e8de2ed672a6072fc37c4f10e0b37a2b301c
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-08-05 12:43:10 +0530

    Create binaryTreeToDLL.cpp

---
+++ Trees/binaryTreeToDLL.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    Node* prev = NULL; Node*  head= NULL;
+
+    public: 
+    //Function to convert binary tree to doubly linked list and return it.
+    
+    Node * bToDLL(Node *root)
+    {
+        // your code here
+        
+        if(root == NULL) return NULL;
+       
+       
+   
+        bToDLL(root->left);
+        
+        if(prev == NULL){
+            prev =root;
+            head = root;
+        }
+        else
+        {
+            root->left = prev;
+            prev->right = root;
+             prev =root;
+        }
+        
+       
+        
+        bToDLL(root->right);
+        
+        
+        return  head;
+        
+        
+        
+    }
+};

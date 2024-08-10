b3c7b47f73707e7a5f47c2003b8c8048f0e75636
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-08-10 13:07:58 +0530

    Create uniqueBinarySearchTrees.cpp

---
+++ Trees/uniqueBinarySearchTrees.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   
+    int t[20];
+
+    int solve(int n)
+    {
+        if( n==0 || n== 1) return 1;
+        if(t[n] != -1) return t[n];
+
+      int ans= 0;
+
+        for(int i=1;i<=n;i++)
+        {
+            ans += (solve(i-1) * solve(n-i));
+        }
+
+        return t[n] = ans;
+    }
+public:
+    int numTrees(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};

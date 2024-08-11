64b6d4f012d7a92bcb08db69b410351bb23b9acf
Prajwal Dahake <97455383+prajwaldd20@users.noreply.github.com>
2024-08-11 14:15:52 +0530

    Create uniquePaths.cpp

---
+++ DynamicProgramming/uniquePaths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+         
+         vector<vector<int>> t(m, vector<int> (n,0));
+
+        // first row -> 1
+         for(int i=0;i<n;i++)
+         {
+            t[0][i] = 1;
+         }
+
+         // first col -> 1
+         for(int i=0;i<m;i++)
+         {
+            t[i][0] =1;
+         }
+
+
+         for(int i=1;i<m;i++)
+         {
+            for(int j=1;j<n;j++)
+            {
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+         }
+
+         return t[m-1][n-1];
+    }
+};
